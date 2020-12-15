cdb35a87e47bda07a42951a0d90e543f724f5603
Martin Fredriksson <martin.fredriksson@canyala.se>
2020-12-15 10:33:13 +0100

    The sed cmd needs some chars to be escaped.

--- create-script-lib.sh
+++ create-script-lib.sh
@@ -106,10 +106,10 @@ rm -f tests/index.js
     echo "console.log('Hello, world from ${NEW_REPO_NAME}');"
 } > main.js
 # Generate .md  
-{}
+# {
 #    echo "# ${NEW_REPO_NAME}" 
 #    echo "**${NEW_REPO_NAME}** is a typescript/assemblyscript enabled node package with debuggable unit tests written in typescript."
-#} > README.md
+# } > README.md
 # Generate .gitignore
 {
     echo 'node_modules'
@@ -210,10 +210,9 @@ mkdir .vscode
 # Update package.json
 GH_AUTHOR=$(git config user.name)
 GH_EMAIL=$(git config user.email)
-PKG_DESCRIPTION="is a typescript/assemblyscript enabled node package with debuggable unittests in typescript."
-PKG_GIT_DESCRIPTION="${NEW_REPO_NAME} ${PKG_DESCRIPTION}"
-PKG_README_DESCRIPTION="**${NEW_REPO_NAME}** ${PKG_DESCRIPTION}"
-echo "\$PKG_README_DESCRIPTION=$PKG_README_DESCRIPTION"
+PKG_GIT_DESCRIPTION="${NEW_REPO_NAME} is a typescript/assemblyscript enabled node package with debuggable unittests in typescript."
+PKG_README_DESCRIPTION="\\*\\*${NEW_REPO_NAME}\\*\\* is a typescript\\/assemblyscript enabled node package with debuggable unittests in typescript\\."
+#echo "\$PKG_README_DESCRIPTION=$PKG_README_DESCRIPTION"
 jq '.version = "0.0.1"' package.json > .tmp && mv .tmp package.json 
 jq '.author = "'"${GH_AUTHOR} <${GH_EMAIL}>"'"' package.json > .tmp && mv .tmp package.json 
 jq '.description = "'"${PKG_GIT_DESCRIPTION}"'"' package.json > .tmp && mv .tmp package.json
@@ -235,12 +234,19 @@ jq '.main = "index.js"' package.json > .tmp && mv .tmp package.json
 # Fetch a copy of the package template and create a README.md 
 cp ~/bin/create-script-lib.template.md ./README.md
 # Replace the macro strings in ./README.md
-sed -i "s/\${NEW_REPO_NAME}/${NEW_REPO_NAME}/" ./README.md
-sed -i "s/\${PKG_README_DESCRIPTON}/${PKG_README_DESCRIPTON}/" ./README.md
-sed -i "s/\${GH_USER}/${GH_USER}/" ./README.md
-sed -i "s/\${GH_AUTHOR}/${GH_AUTHOR}/" ./README.md
-sed -i "s/\${GH_EMAIL}/${GH_EMAIL}/" ./README.md
-sed -i "s/\${NPM_ORG}/${NPM_ORG}/" ./README.md
+NEW_REPO_NAME_MACRO='\\${NEW_REPO_NAME}'
+sed -i "s/${NEW_REPO_NAME_MACRO}/${NEW_REPO_NAME}/" ./README.md
+PKG_README_DESCRIPTION_MACRO='\\${PKG_README_DESCRIPTION}'
+sed -i "s/${PKG_README_DESCRIPTION_MACRO}/${PKG_README_DESCRIPTION}/" ./README.md
+#sed -i "s/${PKG_README_DESCRIPTION_MACRO}/This works ${PKG_DESCRIPTION}/" ./README.md
+GH_USER_MACRO='\\${GH_USER}'
+sed -i "s/${GH_USER_MACRO}/${GH_USER}/" ./README.md
+GH_AUTHOR_MACRO='\\${GH_AUTHOR}'
+sed -i "s/${GH_AUTHOR_MACRO}/${GH_AUTHOR}/" ./README.md
+GH_EMAIL_MACRO='\\${GH_EMAIL}'
+sed -i "s/${GH_EMAIL_MACRO}/${GH_EMAIL}/" ./README.md
+NPM_ORG_MACRO='\\${NPM_ORG}'
+sed -i "s/${NPM_ORG_MACRO}/${NPM_ORG}/" ./README.md
 # GitHub repos Create API call
 echo "Creating repo"
 curl -H "Authorization: token $GH_API_TOKEN" https://api.github.com/user/repos -d '{"name": "'"${NEW_REPO_NAME}"'", "description": "'"${PKG_GIT_DESCRIPTION}"'"}'
